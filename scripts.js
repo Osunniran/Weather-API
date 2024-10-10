6f0bcae2d9368d188596695a25184114068b99d2
AyanfeNifemi <osunniraninioluwa1@gmail.com>
2024-10-10 12:20:33 +0100

    weather

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 let result = ''
+let none = document.getElementById('error')
 let output = document.getElementById('output')
 let inputValue = document.getElementById('input')
 const API_KEY = '7c2190e5d3be1598cce1ea32c34c00a9'
@@ -9,14 +10,19 @@ async function search() {
     console.log('Data is available', data);
 
 
-       result =
-    `
-    <p>${data.name}</p>
-    <p>${data.weather[0].description}</p>
-    <p>${data.main.humidity}</p>
-    <p>${data.main.temp}</p>
-    <p>${data.name}</p>
-    `
+    
 
-    output.innerHTML = result;
+    if (!(`${inputValue.value}`).includes(`${data.name}`) ) {
+        output.innerHTML = `${data.message}`
+    } else {
+        result =
+        `
+        <p><b>City-Name:</b> ${data.name}</p>
+        <p><b>Weather:</b> ${data.weather[0].description}</p>
+        <p><b>Humidity:</b> ${data.main.humidity}</p>
+        <p><b>Tempurature:</b> ${data.main.temp}</p>
+        `
+    
+        output.innerHTML = result;
+    }
 }
